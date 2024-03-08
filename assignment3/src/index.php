c401e59b3afb5e5bd409b73bd49f421a74ac7dad
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2024-03-08 15:52:53 -0700

    [A3] Start on styling

--- assignment3/src/index.php
+++ assignment3/src/index.php
@@ -1,16 +1,25 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>My PHP Web Page</title>
+    <title>Harra and Jrn's PHP Web Page</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
+
 <body>
-    <h1>Welcome to my PHP Web Page!</h1>
-    
-    <?php
-    // Your PHP code goes here
-    echo "Hello, world!";
-    echo "I am Harra";
-    ?>
-    
+    <div class="landing-page">
+        <h1>Welcome Harra and Jrn's 513 Assignment 3</h1>
+        <div class="buttons-container">
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
+
+        <?php
+            // Your PHP code goes here
+            echo "Hello, world!";
+            echo "I am Harra";
+        ?>
+    </div>
 </body>
+
 </html>
