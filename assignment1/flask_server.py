0e770b840e42734e3d7f53d215f60791e854d812
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2024-03-15 01:44:34 -0600

    [A3] Prep for submission

--- assignment1/flask_server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-   return render_template('landing_page_starter.html')
-if __name__ == '__main__':
-   app.run()
